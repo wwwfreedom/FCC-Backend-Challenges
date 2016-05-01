3252a132d3a36173056062250681e898c9273df9
Kevin <tig3r86@gmail.com>
2016-05-01 18:29:35 +0930

    add pre hook logic to encrypt the password before saving the user to db

--- api/models/user.js
+++ api/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt-nodejs')
 
 // Define user model
 const userSchema = new mongoose.Schema({
@@ -19,6 +20,29 @@ const userSchema = new mongoose.Schema({
     picture: { type: String, default: '' }
   }
 }, { timestamps: true })
+
+/**
+ * Password hash middleware (encrypt password)
+ * the pre here means before saving this model run this function
+ */
+// lesson: the top most function can't be arrow function if you want to use 'this'
+ userSchema.pre('save', function (next) {
+  // get access to user model
+  const user = this
+  // step 1: generate a salt
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err) }
+
+    // step 2: hash (encrypt) our password using the salt
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err) }
+      // overwrite plain text password with encrypted password
+      user.password = hash
+      // call next to proceed and save the model
+      next()
+    })
+  })
+ })
 
 // Create the model class
 // create a collection name user using the userSchema for each new documents
